99a581677ff9a785f77e9d9d171b87d61f2da25e
Derek Schuff <dschuff@chromium.org>
2013-10-23 08:35:49 -0700

    Remove obsolete bitcode wrapper code
    We are using our own bitcode reader now, and no longer need this.
    
    R=jvoung@chromium.org, kschimpf@google.com
    BUG=cleanup
    
    Review URL: https://codereview.chromium.org/32943005

--- lib/Wrap/wrapper_output.cpp
+++
@@ -1,9 +0,0 @@
-#include "llvm/Wrap/wrapper_output.h"
-
-bool WrapperOutput::Write(const uint8_t* buffer, size_t buffer_size) {
-  // Default implementation that uses the byte write routine.
-  for (size_t i = 0; i < buffer_size; ++i) {
-    if (!Write(buffer[i])) return false;
-  }
-  return true;
-}
