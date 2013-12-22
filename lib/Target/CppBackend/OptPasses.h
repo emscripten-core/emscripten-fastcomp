1e9d5df67061e962ed148d13258a903f03ce715c
Alon Zakai <alonzakai@gmail.com>
2013-12-22 11:49:39 -0800

    add pass to simplify allocas, so they can better be nativized later

---
+++ lib/Target/CppBackend/OptPasses.h
@@ -0,0 +1,22 @@
+//===-- CPPTargetMachine.h - TargetMachine for the C++ backend --*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef OPT_PASSES_H
+#define OPT_PASSES_H
+
+#include "llvm/Pass.h"
+
+namespace llvm {
+
+  extern FunctionPass *createSimplifyAllocasPass();
+
+} // End llvm namespace
+
+#endif
+
