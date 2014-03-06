emscripten-core/emscripten-fastcomp

commit d95cd364f0c049d6c1b8d78746d44c00ed2f69f2

Optimize out code and variables for no-op pointer casts.

lib/Target/JSBackend/JSBackend.cpp
@@ -998,6 +998,9 @@ std::string JSWriter::getConstant(const Constant* CV, AsmCast sign) {
 }
 
 std::string JSWriter::getValueAsStr(const Value* V, AsmCast sign) {
+  // Skip past no-op bitcasts and zero-index geps.
+  V = V->stripPointerCasts();
+
   if (const Constant *CV = dyn_cast<Constant>(V)) {
     return getConstant(CV, sign);
   } else {
@@ -1006,6 +1009,9 @@ std::string JSWriter::getValueAsStr(const Value* V, AsmCast sign) {
 }
 
 std::string JSWriter::getValueAsCastStr(const Value* V, AsmCast sign) {
+  // Skip past no-op bitcasts and zero-index geps.
+  V = V->stripPointerCasts();
+
   if (isa<ConstantInt>(V) || isa<ConstantFP>(V)) {
     return getConstant(cast<Constant>(V), sign);
   } else {
@@ -1014,6 +1020,9 @@ std::string JSWriter::getValueAsCastStr(const Value* V, AsmCast sign) {
 }
 
 std::string JSWriter::getValueAsParenStr(const Value* V) {
+  // Skip past no-op bitcasts and zero-index geps.
+  V = V->stripPointerCasts();
+
   if (const Constant *CV = dyn_cast<Constant>(V)) {
     return getConstant(CV);
   } else {
@@ -1022,6 +1031,9 @@ std::string JSWriter::getValueAsParenStr(const Value* V) {
 }
 
 std::string JSWriter::getValueAsCastParenStr(const Value* V, AsmCast sign) {
+  // Skip past no-op bitcasts and zero-index geps.
+  V = V->stripPointerCasts();
+
   if (isa<ConstantInt>(V) || isa<ConstantFP>(V)) {
     return getConstant(cast<Constant>(V), sign);
   } else {
@@ -1154,6 +1166,12 @@ static uint64_t LSBMask(unsigned numBits) {
 
 // Generate code for and operator, either an Instruction or a ConstantExpr.
 void JSWriter::generateExpression(const User *I, raw_string_ostream& Code) {
+  // To avoid emiting code and variables for the no-op pointer bitcasts
+  // and all-zero-index geps that LLVM needs to satisfy its type system, we
+  // call stripPointerCasts() on all values before translating them. This
+  // includes bitcasts whose only use is lifetime marker intrinsics.
+  assert(I == I->stripPointerCasts());
+
   Type *T = I->getType();
   if (T->isIntegerTy() && T->getIntegerBitWidth() > 32) {
     errs() << *I << "\n";
@@ -1586,7 +1604,9 @@ void JSWriter::addBlock(const BasicBlock *BB, Relooper& R, LLVMToRelooperMap& LL
   raw_string_ostream CodeStream(Code);
   for (BasicBlock::const_iterator I = BB->begin(), E = BB->end();
        I != E; ++I) {
-    generateExpression(I, CodeStream);
+    if (I->stripPointerCasts() == I) {
+      generateExpression(I, CodeStream);
+    }
   }
   CodeStream.flush();
   const Value* Condition = considerConditionVar(BB->getTerminator());

commit d9003e75353416cfcdb9f03d5fba7e1060a35196

Teach phi translation to also look through bitcasts.
This fixes a regression introduced in d95cd364f0c049d6c1b8d78746d44c00ed2f69f2;
when regular expression translation looks through bitcasts but phi translation
doesn't, phi translation may fail to properly detect dependencies.

lib/Target/JSBackend/JSBackend.cpp
@@ -521,7 +521,10 @@ std::string JSWriter::getPhiCode(const BasicBlock *From, const BasicBlock *To) {
     // we found it
     const std::string &name = getJSName(P);
     assigns[name] = getAssign(P);
-    const Value *V = P->getIncomingValue(index);
+    // Get the operand, and strip pointer casts, since normal expression
+    // translation also strips pointer casts, and we want to see the same
+    // thing so that we can detect any resulting dependencies.
+    const Value *V = P->getIncomingValue(index)->stripPointerCasts();
     values[name] = V;
     std::string vname = getValueAsStr(V);
     if (const Instruction *VI = dyn_cast<const Instruction>(V)) {

test/CodeGen/JS/phi.ll
@@ -1,0 +1,22 @@
+; RUN: llc -march=js < %s | FileCheck %s
+
+; Phi lowering should check for dependency cycles, including looking through
+; bitcasts, and emit extra copies as needed.
+
+; CHECK: while(1) {
+; CHECK:   $k$phi = $j;$j$phi = $k;$k = $k$phi;$j = $j$phi;
+; CHECK: }
+define void @foo(float* nocapture %p, i32* %j.init, i32* %k.init) {
+entry:
+  br label %for.body
+
+for.body:
+  %j = phi i32* [ %j.init, %entry ], [ %k.cast, %more ]
+  %k = phi i32* [ %k.init, %entry ], [ %j.cast, %more ]
+  br label %more
+
+more:
+  %j.cast = bitcast i32* %j to i32*
+  %k.cast = bitcast i32* %k to i32*
+  br label %for.body
+}