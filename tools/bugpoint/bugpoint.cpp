579e5afa5ba320f1f136faa8fdf05bb2fb17c2dc
JF Bastien <jfb@chromium.org>
2015-04-16 09:41:28 -0700

    Remove redundant initialization, seems like a bad merge.

--- tools/bugpoint/bugpoint.cpp
+++ tools/bugpoint/bugpoint.cpp
@@ -193,7 +193,6 @@ int main(int argc, char **argv) {
   initializeLowerEmExceptionsPass(Registry);
   initializeLowerEmSetjmpPass(Registry);
   initializeNoExitRuntimePass(Registry);
-  initializeNoExitRuntimePass(Registry);
   // Emscripten passes end.
   // @LOCALMOD-END
   
