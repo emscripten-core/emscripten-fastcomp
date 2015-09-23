a25b19f4bed44d22687f9738061ffb4f55044e19
Gauthier Billot <gogoprog@gmail.com>
2015-09-23 12:47:21 +0200

    Added line return

--- lib/Target/JSBackend/JSBackend.cpp
+++ lib/Target/JSBackend/JSBackend.cpp
@@ -1364,8 +1364,10 @@ class VectorOperandAccessor
 public:
   static Constant *getOperand(const VectorType *C, unsigned index);
 };
-template<> Constant *VectorOperandAccessor<ConstantVector>::getOperand(const ConstantVector *C, unsigned index) { return C->getOperand(index); }
-template<> Constant *VectorOperandAccessor<ConstantDataVector>::getOperand(const ConstantDataVector *C, unsigned index) { return C->getElementAsConstant(index); }
+template<>
+Constant *VectorOperandAccessor<ConstantVector>::getOperand(const ConstantVector *C, unsigned index) { return C->getOperand(index); }
+template<>
+Constant *VectorOperandAccessor<ConstantDataVector>::getOperand(const ConstantDataVector *C, unsigned index) { return C->getElementAsConstant(index); }
 
 template<typename ConstantVectorType/*= ConstantVector or ConstantDataVector*/>
 std::string JSWriter::getConstantVector(const ConstantVectorType *C) {
