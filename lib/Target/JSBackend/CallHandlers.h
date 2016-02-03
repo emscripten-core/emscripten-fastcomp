7758c8dba9c6bb0f24381f9b843cc79917795136
Jukka Jylänki <jujjyl@gmail.com>
2016-02-03 21:07:03 +0200

    Fix compiler crashes in call handlers for emscripten_int32x4_store3 and emscripten_uint32x4_store3.

--- lib/Target/JSBackend/CallHandlers.h
+++ lib/Target/JSBackend/CallHandlers.h
@@ -943,7 +943,7 @@ DEF_CALL_HANDLER(emscripten_int32x4_store2, {
 })
 DEF_CALL_HANDLER(emscripten_int32x4_store3, {
   UsesSIMDInt32x4 = true;
-  return "SIMD_Int32x4_store3(HEAPU8, " + getValueAsStr(CI->getOperand(0)) + ", " + getValueAsStr(CI->getOperand(1)) + ", " + getValueAsStr(CI->getOperand(2)) + ")";
+  return "SIMD_Int32x4_store3(HEAPU8, " + getValueAsStr(CI->getOperand(0)) + ", " + getValueAsStr(CI->getOperand(1)) + ")";
 })
 DEF_BUILTIN_HANDLER(emscripten_int32x4_load, SIMD_Int32x4_load);
 DEF_CALL_HANDLER(emscripten_int32x4_load1, {
@@ -1019,7 +1019,7 @@ DEF_CALL_HANDLER(emscripten_uint32x4_store2, {
 })
 DEF_CALL_HANDLER(emscripten_uint32x4_store3, {
   UsesSIMDUint32x4 = true;
-  return "SIMD_Uint32x4_store3(HEAPU8, " + getValueAsStr(CI->getOperand(0)) + ", " + getValueAsStr(CI->getOperand(1)) + ", " + getValueAsStr(CI->getOperand(2)) + ")";
+  return "SIMD_Uint32x4_store3(HEAPU8, " + getValueAsStr(CI->getOperand(0)) + ", " + getValueAsStr(CI->getOperand(1)) + ", " + ")";
 })
 DEF_BUILTIN_HANDLER(emscripten_uint32x4_load, SIMD_Uint32x4_load);
 DEF_CALL_HANDLER(emscripten_uint32x4_load1, {
